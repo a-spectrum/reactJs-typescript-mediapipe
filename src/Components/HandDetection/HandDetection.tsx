67ee73cee0f3e8c0773b5d7cd898ac9d0cdc1d6e
Maisie Klaassen <m.klaassen@gillz.nl>
2021-07-08 17:35:14 +0200

    Updated typing

--- src/Components/HandDetection/HandDetection.tsx
+++ src/Components/HandDetection/HandDetection.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, {Ref, useEffect, useState} from "react";
 import Webcam from "react-webcam";
 import {Camera} from '@mediapipe/camera_utils'
-import {HAND_CONNECTIONS, Hands} from '@mediapipe/hands'
-import {drawConnectors, drawLandmarks, lerp} from '@mediapipe/drawing_utils'
+import {HAND_CONNECTIONS, Handedness, Hands} from '@mediapipe/hands'
+import {drawConnectors, drawLandmarks, lerp, NormalizedLandmarkList} from '@mediapipe/drawing_utils'
 
 export enum fingerEnum {
     THUMB_TIP = 4,
@@ -10,6 +10,12 @@ export enum fingerEnum {
     MIDDLE_FINGER_TIP = 12,
     RING_FINGER_TIP = 16,
     PINKY_TIP = 20,
+}
+
+interface fingerCoordinate {
+    x: number;
+    y: number;
+    z: number;
 }
 
 const videoConstraints = {
@@ -20,25 +26,31 @@ const videoConstraints = {
 
 function HandDetection() {
     const handsMirrored = true;
-    const testDivIndexRef: any = React.useRef(null);
-    const testDivThumbRef: any = React.useRef(null);
+    const testDivIndexRef = React.useRef<HTMLDivElement>(null);
+    const testDivThumbRef = React.useRef<HTMLDivElement>(null);
     const [indexDivColour, setIndexDivColour] = useState('blue');
     const [thumbDivColour, setThumbDivColour] = useState('blue');
     const [indexDivBorderRadius, setIndexDivBorderRadius] = useState('50%');
     const [thumbDivBorderRadius, setThumbDivBorderRadius] = useState('50%');
 
-    const webcamRef: any = React.useRef(null);
-    const canvasReference: any = React.useRef();
+    const webcamRef = React.useRef<Webcam>(null);
+    const canvasReference = React.useRef<HTMLCanvasElement>(null);
     const [indexCoordinate, setIndexCoordinate] = useState({x: 0, y: 0, z: 0});
     const [indexLocation, setIndexLocation] = useState({top: 0, bottom: 0, left: 0, right: 0});
     const [thumbCoordinate, setThumbCoordinate] = useState({x: 0, y: 0, z: 0});
     let canvasCtx: any;
-    let camera: any;
+    let camera: Camera;
 
     const between = (x: number, min: number, max: number) => {
         return x >= min && x <= max;
     }
-    const getElementCoordinates = (element: any) => {
+    const getElementCoordinates = (element: React.RefObject<HTMLDivElement>) => {
+        let coordinates = {
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+        };
         if (element.current) {
             const offsets = element.current.getBoundingClientRect();
             setIndexLocation({
@@ -47,31 +59,34 @@ function HandDetection() {
                 left: offsets.left,
                 right: offsets.right
             });
-            return {
-                top: offsets.top,
-                bottom: offsets.bottom,
-                left: offsets.left,
-                right: offsets.right
+
+            coordinates.top = offsets.top;
+            coordinates.bottom = offsets.bottom;
+            coordinates.left = offsets.left;
+            coordinates.right = offsets.right;
             };
-        }
+
+        return coordinates;
+
     }
-    const calculateCoordinate = (landmarks: Array<any>, finger: fingerEnum) => {
-        let fingerCoordinate;
+
+    const calculateCoordinate = (landmarks: Array<object>, finger: fingerEnum) => {
+        let fingerCoordinate: fingerCoordinate;
         switch (finger) {
             case fingerEnum.PINKY_TIP:
-                fingerCoordinate = landmarks[fingerEnum.PINKY_TIP];
+                fingerCoordinate = landmarks[fingerEnum.PINKY_TIP] as fingerCoordinate;
                 break;
             case fingerEnum.RING_FINGER_TIP:
-                fingerCoordinate = landmarks[fingerEnum.RING_FINGER_TIP];
+                fingerCoordinate = landmarks[fingerEnum.RING_FINGER_TIP] as fingerCoordinate;
                 break;
             case fingerEnum.MIDDLE_FINGER_TIP:
-                fingerCoordinate = landmarks[fingerEnum.MIDDLE_FINGER_TIP];
+                fingerCoordinate = landmarks[fingerEnum.MIDDLE_FINGER_TIP] as fingerCoordinate;
                 break;
             case fingerEnum.INDEX_FINGER_TIP:
-                fingerCoordinate = landmarks[fingerEnum.INDEX_FINGER_TIP];
+                fingerCoordinate = landmarks[fingerEnum.INDEX_FINGER_TIP] as fingerCoordinate;
                 break;
             case fingerEnum.THUMB_TIP:
-                fingerCoordinate = landmarks[fingerEnum.THUMB_TIP];
+                fingerCoordinate = landmarks[fingerEnum.THUMB_TIP] as fingerCoordinate;
                 break;
         }
         return {
@@ -79,14 +94,14 @@ function HandDetection() {
             y: fingerCoordinate.y * window.innerHeight,
         };
     }
-    const isElementTouchedByFinger = (element: any, landmarks: Array<any>, finger: fingerEnum) => {
+    const isElementTouchedByFinger = (element: React.RefObject<HTMLDivElement>, landmarks: Array<Array<object>>, finger: fingerEnum) => {
         // Get coordinates of element
         let touched = false;
-        const elementCoordinates: any = getElementCoordinates(element);
+        const elementCoordinates: { top: number; left: number; bottom: number; right: number } = getElementCoordinates(element);
 
         for (let index = 0; index < landmarks.length; index++) {
             // Get coordinates of finger tip of specific finger.
-            const landmarkCoordinates: any = calculateCoordinate(landmarks[index], finger);
+            const landmarkCoordinates: { x: number; y: number } = calculateCoordinate(landmarks[index], finger);
             touched === false && (touched =
                     (
                         between(landmarkCoordinates.x, elementCoordinates.left, elementCoordinates.right)
@@ -165,49 +180,52 @@ function HandDetection() {
         </div>
     }
 
-    const onResults = (results: any) => {
+    const onResults = (results: { multiHandLandmarks?: NormalizedLandmarkList[] | undefined; multiHandedness?: Handedness[] | undefined; }) => {
         // console.log(results)
-        canvasCtx = canvasReference.current.getContext('2d');
-        canvasCtx.width = webcamRef.current.video.videoWidth;
-        canvasCtx.height = webcamRef.current.video.videoHeight;
-        canvasCtx.save();
-        canvasCtx.clearRect(0, 0, canvasReference.current.width, canvasReference.current.height);
-        // Webcam feed again
-        // canvasCtx.drawImage(results.image, 0, 0, canvasReference.current.width, canvasReference.current.height);
 
-        canvasCtx.fillStyle = "black";
-        canvasCtx.fillRect(0, 0, canvasCtx.width, canvasCtx.height);
-        let fingerCoordinatesList: Array<any> = [];
+        canvasReference.current && (canvasCtx = canvasReference.current.getContext('2d'));
+        if(canvasCtx) {
+            webcamRef.current && webcamRef.current.video && (canvasCtx.width = webcamRef.current.video.videoWidth);
+            webcamRef.current && webcamRef.current.video && (canvasCtx.height = webcamRef.current.video.videoHeight);
+            canvasCtx.save();
+            canvasReference.current && canvasCtx.clearRect(0, 0, canvasReference.current.width, canvasReference.current.height);
+            // Webcam feed again
+            // canvasCtx.drawImage(results.image, 0, 0, canvasReference.current.width, canvasReference.current.height);
 
-        if (results.multiHandLandmarks && results.multiHandedness) {
-            for (let index = 0; index < results.multiHandLandmarks.length; index++) {
-                const classification = results.multiHandedness[index];
-                const isRightHand = classification.label === 'Right';
-                const landmarks = results.multiHandLandmarks[index];
+            canvasCtx.fillStyle = "black";
+            canvasCtx.fillRect(0, 0, canvasCtx.width, canvasCtx.height);
+            let fingerCoordinatesList: Array<Array<object>> = [];
 
-                drawConnectors(
-                    canvasCtx, landmarks, HAND_CONNECTIONS,
-                    {color: isRightHand ? '#00FF00' : '#FF0000'});
+            if (results.multiHandLandmarks && results.multiHandedness) {
+                for (let index = 0; index < results.multiHandLandmarks.length; index++) {
+                    const classification = results.multiHandedness[index];
+                    const isRightHand = classification.label === 'Right';
+                    const landmarks = results.multiHandLandmarks[index];
 
-                drawLandmarks(canvasCtx, landmarks, {
-                    color: isRightHand ? '#00FF00' : '#FF0000',
-                    fillColor: isRightHand ? '#FF0000' : '#00FF00',
-                    radius: (x) => {
-                        // @ts-ignore
-                        return lerp(x.from?.z, -0.15, .1, 1, 1);
-                    }
-                });
+                    drawConnectors(
+                        canvasCtx, landmarks, HAND_CONNECTIONS,
+                        {color: isRightHand ? '#00FF00' : '#FF0000'});
 
-                fingerCoordinatesList.push(landmarks);
+                    drawLandmarks(canvasCtx, landmarks, {
+                        color: isRightHand ? '#00FF00' : '#FF0000',
+                        fillColor: isRightHand ? '#FF0000' : '#00FF00',
+                        radius: (x) => {
+                            // @ts-ignore
+                            return lerp(x.from?.z, -0.15, .1, 1, 1);
+                        }
+                    });
+
+                    fingerCoordinatesList.push(landmarks);
+                }
+
+                if (fingerCoordinatesList.length > 0) {
+                    isElementTouchedByFinger(testDivIndexRef, fingerCoordinatesList, fingerEnum.INDEX_FINGER_TIP);
+                    isElementTouchedByFinger(testDivThumbRef, fingerCoordinatesList, fingerEnum.THUMB_TIP);
+                }
+
             }
-
-            if (fingerCoordinatesList.length > 0) {
-                isElementTouchedByFinger(testDivIndexRef, fingerCoordinatesList, fingerEnum.INDEX_FINGER_TIP);
-                isElementTouchedByFinger(testDivThumbRef, fingerCoordinatesList, fingerEnum.THUMB_TIP);
-            }
-
+            canvasCtx.restore();
         }
-        canvasCtx.restore();
     }
 
     useEffect(() => {
@@ -238,7 +256,7 @@ function HandDetection() {
                 height: 720,
             });
 
-            camera.start();
+            camera.start().then();
         }
     }, []);
 
